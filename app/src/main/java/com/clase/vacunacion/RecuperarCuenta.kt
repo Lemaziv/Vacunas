cf66f78f0ba000dc8791c4f2a79a0a867727de67
Lemaziv <vitt99@hotmail.com>
2022-03-29 01:33:22 -0500

    Initial commit

---
+++ app/src/main/java/com/clase/vacunacion/RecuperarCuenta.kt
@@ -0,0 +1,11 @@
+package com.clase.vacunacion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecuperarCuenta : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.recuperar_cuenta)
+    }
+}
